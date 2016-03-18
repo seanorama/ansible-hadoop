fbe3bbd2521bca1a55ee11a9328750dd354e2a20
Sean Roberts <seanorama@gmail.com>
2016-03-18 20:22:39 +0000

    cleanup to match bootstrap_aws.sh

--- hortonworks_aws.sh
+++ hortonworks_aws.sh
@@ -1,27 +1,31 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-export AWS_ACCESS_KEY_ID="$(grep -i AWS_ACCESS_KEY_ID ~/.aws/credentials | cut -d= -f2 | xargs)"
-export AWS_SECRET_ACCESS_KEY="$(grep -i AWS_SECRET_ACCESS_KEY ~/.aws/credentials | cut -d= -f2 | xargs)"
+#export AWS_ACCESS_KEY_ID="$(grep -i AWS_ACCESS_KEY_ID ~/.aws/credentials | cut -d= -f2 | xargs)"
+#export AWS_SECRET_ACCESS_KEY="$(grep -i AWS_SECRET_ACCESS_KEY ~/.aws/credentials | cut -d= -f2 | xargs)"
 
 aws_region=$(grep aws_region playbooks/group_vars/all|cut -d"'" -f2)
 cluster_name=$(grep cluster_name playbooks/group_vars/all|cut -d"'" -f2)
+destination_variable=${destination_veriable:-public_dns_name} ## easy override
 
-# Uncomment this for Ubuntu Builds
-# ansible_user="ubuntu"
+# Set from the environment before running, or uncomment below
+ansible_user=${ansible_user:-centos}
+#ansible_user=centos" ## CentOS
+#ansible_user="ubuntu" ## Ubuntu
+#ansible_user="ec2-user" ## Amazon Linux, RedHat
 
-# Uncomment this for Redhat/CentOS builds
-ansible_user="ec2-user"
 
 cat > inventory/ec2.ini <<EOL
 [ec2]
-regions = $aws_region
+regions = ${aws_region}
 regions_exclude =
-instance_filters = tag:environment=$cluster_name
+instance_filters = tag:environment=${cluster_name}
 cache_path = ~/.ansible/tmp
 cache_max_age = 1
-destination_variable = public_dns_name
+destination_variable = ${destination_variable}
 vpc_destination_variable = ip_address
 
+elasticache = False
+rds = False
 route53 = False
 all_instances = False
 all_rds_instances = False
@@ -36,5 +40,5 @@ group_by_tag_keys = True
 group_by_security_group = True
 EOL
 
-ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook  -e "ansible_user=$ansible_user" -i inventory/ec2.py playbooks/hortonworks_aws.yml
+ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -e "cluster_interface=eth0 ansible_user=${ansible_user} os.firewall=false data_disks_devices=[]" -i inventory/ec2.py playbooks/hortonworks_aws.yml
 
