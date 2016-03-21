f6bc7e198899885d2c80da3ae80638860523ece1
Sean Roberts <seanorama@gmail.com>
2016-03-21 20:17:15 +0000

    simplify aws

--- bootstrap_aws.sh
+++ bootstrap_aws.sh
@@ -3,10 +3,4 @@
 #export AWS_ACCESS_KEY_ID="$(grep -i AWS_ACCESS_KEY_ID ~/.aws/credentials | cut -d= -f2 | xargs)"
 #export AWS_SECRET_ACCESS_KEY="$(grep -i AWS_SECRET_ACCESS_KEY ~/.aws/credentials | cut -d= -f2 | xargs)"
 
-# Set from the environment before running, or uncomment below
-ansible_user=${ansible_user:-ec2-user}
-#ansible_user=centos" ## CentOS
-#ansible_user="ubuntu" ## Ubuntu
-#ansible_user="ec2-user" ## Amazon Linux, RedHat
-
-ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -e "ansible_user=${ansible_user}" -i inventory/aws/ playbooks/bootstrap.yml -vvvv
+ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -i inventory/aws/ playbooks/bootstrap.yml
--- hortonworks_aws.sh
+++ hortonworks_aws.sh
@@ -3,11 +3,5 @@
 #export AWS_ACCESS_KEY_ID="$(grep -i AWS_ACCESS_KEY_ID ~/.aws/credentials | cut -d= -f2 | xargs)"
 #export AWS_SECRET_ACCESS_KEY="$(grep -i AWS_SECRET_ACCESS_KEY ~/.aws/credentials | cut -d= -f2 | xargs)"
 
-# Set from the environment before running, or uncomment below
-ansible_user=${ansible_user:-ec2-user}
-#ansible_user=centos" ## CentOS
-#ansible_user="ubuntu" ## Ubuntu
-#ansible_user="ec2-user" ## Amazon Linux, RedHat
-
-ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -e "ansible_user=${ansible_user}" -i inventory/aws/ playbooks/hortonworks.yml -vvvv
+ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -i inventory/aws/ playbooks/hortonworks.yml
 
